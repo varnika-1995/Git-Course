c0204139bdbaa70f8b9222d16bbbb8c5d73562e9
varnika <varnikareddyguduru@MacBookAir.lan>
2025-04-15 19:29:53 -0500

    story3.1 -user input

--- FirstCode.java
+++ FirstCode.java
@@ -2,4 +2,7 @@ public class FirstCode
 {
 public static void main(String args[]){
 System.out.println("hello world!!!!");
+System.out.println("Welcome to my page");
+System.out.println("Learning about GIT");
+
 }}
